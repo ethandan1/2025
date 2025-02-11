0fb41f12a6375794791d9e7e3295e0836fcd32de
ethandan1 <49495915+ethandan1@users.noreply.github.com>
2025-02-11 01:46:48 -0700

    Add files via upload
    So these are all the things I did at first...(2 hours of work)
    
    1. Fixed Pair_Sort() logic
    2. Added is_valid_deck() function to alert user when invalid or duplicate cards exist
    3. Wrote compare_hands() to determine winner between two 5-card hands
    4. Added a Board class
    5.  Added append() methods to Hand and Board class since you normally can't append to objects
    6.  Updated Testing Logic so Harry can find edge cases to his heart's content
    THEN...
    8.  Attempted adjusting so that everything uses Hand and Card objects rather than string representations of cards, this would allow for your .append(cards) thing without using a str(card) and I agree this is how it should be across the board. This was awful, and after 3 hours of updating and debugging later the code runs and everything is now hunkey dorey. Go test to your heart's content Harry

--- Ultimate_Texas_Holdem.py
+++ Ultimate_Texas_Holdem.py
@@ -3,8 +3,12 @@ import random
 from collections import Counter
 from itertools import combinations
 
+## TO DO:
+# https://www.youtube.com/watch?v=mENr3b4IlqQ&ab_channel=StackWild
+
+
 class Card:
-    values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    values = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
     suits = ['h','d','c','s']
 
     def __init__(self, value, suit):
@@ -12,7 +16,7 @@ class Card:
         self.suit = suit
     
     def __str__(self):
-        return self.value + self.suit
+        return f"{self.value}{self.suit}"
 
 class Deck:
     def __init__(self):
@@ -25,37 +29,58 @@ class Deck:
         return ', '.join(str(card) for card in self.cards)
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        n = len(self.cards)
+        for i in range(n - 1, 0, -1):
+            j = random.randint(0, i)
+            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
+        return self
 
     def deal_card(self):
         if not self.cards:
             raise ValueError("No Cards Exist")
         return self.cards.pop()
 
-    def reset(self):
-        'resets deck to unshuffled state'
-        self.__init__()
+class Board:
+    def __init__(self, cards):
+        self.cards = cards
 
+    def append(self, card):
+        self.cards.append(card)
+
+    def __str__(self):
+        return f"[{', '.join(str(card) for card in self.cards)}]"
+    
 class Hand:
     def __init__(self, cards):
         self.cards = cards
-        self.strength = strength
+
+    def append(self, card):
+        self.cards.append(card)
     
     def __str__(self):
-        return ', '.join(str(card) for card in self.cards)
-
-    def evaluate_hand(self):
-        hand_ranking = ['AsKsQsJsTs','AhKhQhJhTh']
+        return f"[{', '.join(str(card) for card in self.cards)}]"
+    
+    def evaluate_hand(self, hand, board):
+        hand_score = best_hand(hand+board)[0]
+        return hand_score
         
 class Player:
-    def __init__(self, balance, hand):
+    def __init__(self, balance, hand, bets):
         self.balance = balance
         self.hand = hand
+        self.bets = bets
         self.current_decision = None
     
     def make_decision(self, decision):
         self.current_decision = decision
-        
+
+class Dealer:
+    def __init__(self, hand):
+        self.hand = hand
+    
+    def qualifies(self, hand, board):
+        return evaluate_hand(hand, board) >= 1
+
 class Bets:
     def __init__(self, ante = 0, blind = 0, trips = 0, progressive = 0):
         self.ante = ante
@@ -98,73 +123,75 @@ class Game:
 
 
 
-### STAND ALONE EVALUATOR FUNCTIONS
+### Main Hand Scoring Functions
 
-def highest_card(cards):
-    values = "23456789TJQKA" 
-    highest_card = max(cards, key=lambda card: values.index(card[0]))
-    return highest_card[0]
+def compare_hands(player_hand, dealer_hand, board):
 
-def sum_of_kickers(hand):
-    values = [c[0] for c in hand]
-    counts = Counter(values)
-    kickers = [v for v in values if counts[v] == 1]
-    return sum(card_vals(kickers))
+    all_cards = player_hand.cards + dealer_hand.cards + board.cards
 
-def num_of_kind(cards):
-    return Counter(c[0] for c in cards)
+    if not is_valid_deck(all_cards)[0]:
+        raise ValueError(f"Invalid cards detected: duplicates or non-existent card: '{is_valid_deck(all_cards)[1]}'")
 
-def count_pairs(cards):
-    return sum(i > 1 for i in num_of_kind(cards).values())
+    player_result = best_hand(player_hand.cards + board.cards)
+    dealer_result = best_hand(dealer_hand.cards + board.cards)
 
-def largest_pair(cards):
-    return max(num_of_kind(cards).values())
+    player_score, player_best_hand, player_description = player_result
+    dealer_score, dealer_best_hand, dealer_description = dealer_result
 
-def is_straight(cards):
-    values = [c[0] for c in cards]
-    index = "A23456789TJQKA"["K" in values:].index
-    indices = sorted(index(v) for v in values)
-    return all(x == y for x, y in enumerate(indices, indices[0]))
+    player_summary = f"Player: {player_best_hand}: {player_description}"
+    dealer_summary = f"Dealer: {dealer_best_hand}: {dealer_description}"
 
-def is_flush(cards):
-    suit_pop = Counter(c[1] for c in cards)
-    return any(s > 4 for s in suit_pop.values())
+    if player_score > dealer_score:
+        winner = "Player"
+    elif player_score < dealer_score:
+        winner = "Dealer"
+    
+    else:
+        for player_card, dealer_card in zip(player_best_hand.cards, dealer_best_hand.cards):
 
-def straight_sort(cards):
-    values = [c[0] for c in cards]
-    index = "A23456789TJQKA"["K" in values:].index
-    return sorted(cards, key=lambda x:index(x[0]), reverse=True)
+            player_card_value = card_vals([player_card])[0]
+            dealer_card_value = card_vals([dealer_card])[0]
 
-def flush_sort(cards):
-    suit_pop = Counter(c[1] for c in cards)
-    return sorted(cards, key=lambda x: suit_pop[x[1]], reverse=True)
+            if player_card_value > dealer_card_value:
+                winner = "Player"
+                break
+            elif player_card_value < dealer_card_value:
+                winner = "Dealer"
+                break
+            else:
+                winner = "Tie"
+    
+    player_best_hand_str = [str(card) for card in player_best_hand.cards] 
+    dealer_best_hand_str = [str(card) for card in dealer_best_hand.cards]
 
-def pair_sort(cards):
-    num = num_of_kind(cards)
-    return sorted(cards, key=lambda x: num[x[0]], reverse=True)
+    # Compare hands currently returns strings of descriptions and of player and dealer hands rather than the card objects themselves
+    # This is for testing/display purposes only and if we need to use this output elsewhere as an input it should keep convention as an object  
 
-def card_vals(cards):
-    """Converts card values to corresponding integers for comparison."""
-    value_map = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, 
-                 "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
-    return [value_map[card[0]] for card in cards]
+    return winner, 'Wins!',' Player: ', player_best_hand_str, player_description, ' Dealer: ', dealer_best_hand_str, dealer_description
 
-def first_card(cards):
-    return cards[0][0]
+def best_hand(cards):
+    all_hands = list(combinations(cards, 5))
+    all_hands = [Hand(list(hand)) for hand in all_hands]
 
-def second_card(cards):
-    return cards[1][0]
+    hand_scores = [score_hand(hand)[0] for hand in all_hands]
+    hand_results = [score_hand(hand)[2] for hand in all_hands]
 
-def third_card(cards):
-    return cards[2][0]
+    hands_sorted_by_score = list(zip(hand_scores, hand_results))
 
-def fourth_card(cards):
-    return cards[3][0]
+    best_score = max(hands_sorted_by_score, key=lambda x: x[0])[0]
+    best_hands = [hand for score, hand in hands_sorted_by_score if score == best_score]
+    best_hands_sorted_by_kicker_sum = sorted(best_hands, key=lambda hand: sum_of_kickers(Hand(hand)), reverse=True)
 
-def fifth_card(cards):
-    return cards[4][0]
+    best_hand = Hand(best_hands_sorted_by_kicker_sum[0])
+    hand_score, _, _, hand_description = score_hand(best_hand)
 
-def score_hand(cards):
+    return hand_score, best_hand, hand_description
+
+
+
+def score_hand(hand):
+    cards = hand.cards
+
     pairs = count_pairs(cards)
     largest = largest_pair(cards)
 
@@ -200,40 +227,133 @@ def score_hand(cards):
             hand_description = f"Two-Pair: {first_card(cards)}s and {third_card(cards)}s {fifth_card(cards)} kicker"
         elif hand_score == 1:
             hand_description = f"Pair of {first_card(cards)}s {third_card(cards)} kicker"
+        else:
+            hand_description = f"{first_card(cards)} High"
         
 
     return hand_score, card_vals(cards), cards, hand_description
 
-def best_hand(cards):
-    all_hands = list(combinations(cards, 5)) 
-    scored_hands = [(score_hand(hand)[0], score_hand(hand)[2]) for hand in all_hands] 
-
-    best_score = max(scored_hands, key=lambda x: x[0])[0] 
-
-    best_hands = [hand for score, hand in scored_hands if score == best_score]  
-
-    best_hands_sorted = sorted(best_hands, key=lambda hand: sum_of_kickers(hand), reverse=True) 
-
-
-    best_hand = best_hands_sorted[0]
-    hand_score, _, _, hand_description = score_hand(best_hand) 
-
-    return best_hand, hand_description
 
 
 
-hand = ['As', 'Ad']
-board = ['Js', 'Td', '6h', '4s', 'Qs']
-print(*best_hand(hand+board))
+#### Helper Evaluator Functions 
+
+def create_card(card_str):
+    value = card_str[:-1]  # All but last character for value
+    suit = card_str[-1]    # Last character for suit
+    return Card(value, suit)
 
 
+def is_valid_deck(cards):
+    valid_ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    valid_suits = ['h', 'd', 'c', 's']
+
+    seen_cards = set()
+    for card in cards:
+        card_str = str(card)  
+        if card_str in seen_cards:
+            return False, card 
+
+        seen_cards.add(card_str)
+        
+        if card.value not in valid_ranks or card.suit not in valid_suits:
+            return False, card
+    return True, card
 
 
+def highest_card(cards):
+    values = "23456789TJQKA" 
+    highest_card = max(cards, key=lambda card: values.index(card.value))
+    return highest_card.value
+
+def sum_of_kickers(hand):
+    values = [c.value for c in hand.cards]
+    counts = Counter(values)
+    kickers = [c for c in hand.cards if counts[c.value] == 1]
+    return sum(card_vals(kickers))
+
+def num_of_kind(cards):
+    return Counter(c.value for c in cards)
+
+def count_pairs(cards):
+    return sum(i > 1 for i in num_of_kind(cards).values())
+
+def largest_pair(cards):
+    return max(num_of_kind(cards).values())
+
+def is_straight(cards):
+    values = [c.value for c in cards]
+    index = "A23456789TJQKA"["K" in values:].index
+    indices = sorted(index(v) for v in values)
+    return all(x == y for x, y in enumerate(indices, indices[0]))
+
+def is_flush(cards):
+    suit_pop = Counter(c.suit for c in cards)
+    return any(s > 4 for s in suit_pop.values())
+
+def straight_sort(cards):
+    values = [c.value for c in cards]
+    index = "A23456789TJQKA"["K" in values:].index
+    return sorted(cards, key=lambda x:index(x.value), reverse=True)
+
+def flush_sort(cards):
+    suit_pop = Counter(c.suit for c in cards)
+    return sorted(cards, key=lambda x: suit_pop[x.suit], reverse=True)
+
+def pair_sort(cards):
+    num = num_of_kind(cards)  
+    return sorted(cards, key=lambda x: (num[x.value], card_vals([x])[0]), reverse=True)
 
 
-# deck = Deck()
-# deck.shuffle()
-# print(deck)
-# for card in deck:
-#     card = deck.deal_card()
-#     print(card)
+def card_vals(cards):
+    """Converts card values to corresponding integers for comparison."""
+    value_map = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, 
+                 "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+    return [value_map[card.value] for card in cards]
+
+def first_card(cards):
+    return cards[0].value
+
+def second_card(cards):
+    return cards[1].value
+
+def third_card(cards):
+    return cards[2].value
+
+def fourth_card(cards):
+    return cards[3].value
+
+def fifth_card(cards):
+    return cards[4].value
+
+# player_cards = ['As', 'Kd']
+# dealer_cards = ['Ad', 'Kc']
+# board_cards = ['Jd', 'Qd', '9h', 'Ts', '3h']
+
+# player_hand = Hand([create_card(card) for card in player_cards])
+# dealer_hand = Hand([create_card(card) for card in dealer_cards])
+# board = [create_card(card) for card in board_cards]
+
+deck = Deck()
+deck.shuffle()
+
+player_hand = Hand([])
+dealer_hand = Hand([])
+board = Board([])
+
+for i in range(9):  
+    card = deck.deal_card()
+    if i > 3:  
+        board.append(card)
+    elif i % 2 == 0: 
+        player_hand.append(card)
+    elif i % 2 == 1: 
+        dealer_hand.append(card)
+
+print(f"Player: {player_hand}, Dealer: {dealer_hand}, Board: {board}")
+print(*compare_hands(player_hand, dealer_hand, board))
+
+# hand = [Card('2','c'), Card('8','h'), Card('J','s'), Card('2','d'), Card('K','h')]
+# sorted_hand = pair_sort(hand)
+# print(f"{sorted_hand}")  # Output the sorted list of cards
+
